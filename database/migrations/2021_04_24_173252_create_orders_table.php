8f16cc44130a443eda2b107b3c88ba8987f64590
shilov <shilov.a.a.1986@gmail.com>
2021-05-07 00:32:09 +0700

    создал миграцию и сидр расценки

--- database/migrations/2021_04_24_173252_create_orders_table.php
+++ database/migrations/2021_04_24_173252_create_orders_table.php
@@ -20,7 +20,23 @@ class CreateOrdersTable extends Migration
                 $table->integer('dept');
                 $table->string('name', 120);
                 $table->enum('status', ['готов', 'в работе']);
-                $table->float('product_area', 8, 2);
+                $table->float('product_area', 8, 2); //Площадь изделия
+                $table->float('sliding', 8, 2); // Раздвижка
+                $table->float('blind', 8, 2); // Глухарь
+                $table->float('impost', 8, 2); // Импост
+                $table->float('P400/02', 8, 2); // Р400/02
+                $table->float('blind_angle', 8, 2); // Глухарь углы
+                $table->float('m/n', 8, 2); // м/с
+                $table->float('door_mono_cold', 8, 2); // Дверь хол одно створка
+                $table->float('door_double_cold', 8, 2); // Дверь хол двух створка
+                $table->float('stained_glass_cold', 8, 2); // Витраж холодный
+                $table->float('door_mono_warm', 8, 2); // Дверь тёплая одно створка
+                $table->float('door_double_warm', 8, 2); // Дверь тёплая двух створка
+                $table->float('stained_glass_warm', 8, 2); // Витраж тёплый
+                $table->float('non-standard', 8, 2); // Не стандарт
+                $table->float('facade', 8, 2); // Фасад
+                $table->float('sandwich', 8, 2); // Сэндвич
+                $table->float('cost', 8, 2); // Стоимость заказа
                 $table->timestamps();
             });
     }
---
+++ database/migrations/2021_05_06_133401_create_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string(50, 'job_name');
+            $table->integer('price_of_work');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
